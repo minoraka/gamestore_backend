42a37ec80c2d26fcfba052135b1052bb33e5ac1b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-03-06 10:47:26 +0600

    bugs fixed and now ts works

--- store/models.py
+++ store/models.py
@@ -5,18 +5,30 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name 
+
 
 class Platform(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name 
 
 
 class Publisher(models.Model):
     name = models.CharField(max_length=255)
     website = models.URLField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name 
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name 
 
 
 class Game(models.Model):
@@ -33,6 +45,9 @@ class Game(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name 
+
 
 class Discount(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
@@ -41,6 +56,9 @@ class Discount(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
+    def __str__(self):
+        return self.name 
+
 
 class DLC(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
@@ -48,14 +66,23 @@ class DLC(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     release_date = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name 
+
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_paid = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name 
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.name 
